ba8db78896e6b203a7617d4874ab815888b9bbe4
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-07 10:46:30 +0100

    adding cv

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
         <div className="App-header">
           <h2 className="App-title">Dan Adeyemi</h2>
           <p className="App-tagline">Creative Technologist Specialising in all things digital</p>
-          <a href="mailto:info@danadeyemi.com" className="App-link App-email">Email</a>
           <a href="https://www.instagram.com/iamdeaneyelid/" className="App-link App-instagram" target="_blank">Instagram</a>
           <a href="https://twitter.com/iamdeaneyelid" className="App-link App-twitter" target="_blank">Twitter</a>
           <a href="https://uk.linkedin.com/in/danadeyemi" className="App-link App-twitter" target="_blank">LinkedIn</a>
+          <a href="mailto:info@danadeyemi.com" className="App-link App-email">Email</a>
         </div>
+        <div className="App-footer">
+          <a href="/src/files/dan-adeyemi-cv.pdf" className="App-link App-cv" target="_blank">CV</a>
+        </div> 
       </div>
     );
   }
