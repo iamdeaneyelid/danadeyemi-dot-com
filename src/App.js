8f82eda91bef21ab473c3576192d2d6e6e6d0fe8
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-21 23:41:32 +0100

    trying to sep

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { Router, Route, Link } from 'react-router'
-import './js/vendor/analytics.js'
+import './js/vendor/analytics'
 import home from './js/components/home'
 import { browserHistory } from 'react-router'
-import axios from 'axios'
-import contentful from 'contentful'
-var client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: '5jz2ccsep5wg',
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: 'a8e28687058c38ce699d1f2f44545a7c4735944484900d7548b85f7614aed31e'
-})
+// import axios from 'axios'
+import { test } from './js/libs/contentful-client'
 
+// import contentful from 'contentful'
+// var client = contentful.createClient({
+//   space: '5jz2ccsep5wg',
+//   accessToken: 'a8e28687058c38ce699d1f2f44545a7c4735944484900d7548b85f7614aed31e'
+// })
 
 class App extends Component {
  
@@ -24,16 +23,18 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getData()
+    console.log('clientsss',test)
+    // this.getData()
   }
   
   getData = () => {
+    console.log(client)
     client.getEntries()
     .then((entries) => {
-        this.setState({data:entries})
+      this.setState({data:entries})
     })
   }
-
+  
   // getData = () => {
   //   axios.get('/path/to/user-api').then(function(response) {
   //     this.setState({data: response})
---
+++ src/js/libs/contentful-client.js
@@ -0,0 +1,16 @@
+//import contentful from 'contentful'
+
+
+// var client = contentful.createClient({
+//   space: '5jz2ccsep5wg',
+//   accessToken: 'a8e28687058c38ce699d1f2f44545a7c4735944484900d7548b85f7614aed31e'
+// })
+
+
+var test = 10;
+
+console.log('client within', test)
+
+export var test
+
+
