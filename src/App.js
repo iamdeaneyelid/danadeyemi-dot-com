50badbf98e0e01f3658964b6400fc63fe539982c
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-21 20:11:46 +0100

    renames

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { Router, Route, Link } from 'react-router'
 import './js/vendor/analytics.js'
-import Home from './js/components/home'
+import home from './js/components/home'
 import { browserHistory } from 'react-router'
+import axios from 'axios'
 
 
 class App extends Component {
+ 
+  constructor(props) {
+    super()
+    this.props = props
+    this.state = {
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData = () => {
+    axios.get('/path/to/user-api').then(function(response) {
+      this.setState({data: response})
+    });
+  }
+
   render() {
     return (
     <Router history={browserHistory}>
-      <Route path="/" component={Home} />
+      <Route path="/" component={home} />
     </Router>
     );
   }
