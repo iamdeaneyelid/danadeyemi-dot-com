cf71980fef2dfaa1075a7df7a3e6afc7d1d53372
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-26 22:31:40 +0100

    most of the mobile styling

--- src/js/components/sidebar/sidebar.js
+++ src/js/components/sidebar/sidebar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './sidebar.css';
 import Header from '../header/header'
 import ProjectList from '../projectList/projectList'
+import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router'
+
+import Radium from 'radium';
+
+let RadiumLink = Radium(Link);
 
 class Sidebar extends Component {
 
@@ -10,10 +16,24 @@ class Sidebar extends Component {
     this.props = props
   }
 
+  closeMenu = () => {
+    Menu.isOpen={false}
+  }
+
   render() {
     return (
       <div className="sidebar">
-        <Header />
+        <Menu id={"header-mobile-menu"} right isOpen={false}>
+          <p className="header-mobile-project-header project-header">Work</p>
+           {
+            this.props.projects.map(function(project) {
+              return(
+                <RadiumLink className="header-mobile-menu-item project-link" to={'/project/' + project.fields.slug} onClick={this.closeMenu} key={project.sys.id} activeClassName="header-mobile-menu-item-active">{project.fields.title}</RadiumLink>
+              )
+            }.bind(this))
+          }
+        </Menu>
+        <Header {...this.props}/>
         <ProjectList {...this.props}/>        
       </div> 
     );
