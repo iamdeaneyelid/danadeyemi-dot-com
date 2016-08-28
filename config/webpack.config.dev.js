c83e5323607ec797c1e05bc7b63761d443751bc6
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-28 21:52:10 +0100

    removing case sensitive plugin from webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,7 +2,6 @@ var path = require('path');
 var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 var paths = require('./paths');
 
 module.exports = {
@@ -98,6 +97,5 @@ module.exports = {
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' }),
     // Note: only CSS is currently hot reloaded
     new webpack.HotModuleReplacementPlugin(),
-    new CaseSensitivePathsPlugin()
   ]
 };
