b99c5ec6913ac36581f6b0f92315a7cedacc2b50
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-28 23:50:16 +0100

    changing about text and project arrow position

--- src/js/components/aboutModal/aboutModal.js
+++ src/js/components/aboutModal/aboutModal.js
@@ -26,18 +26,13 @@ class AboutModal extends Component {
               <p className="modal-date">b.1990</p>
               <div className="modal-description">
                 <p className="modal-description-text">
-                  Dan Adeyemi is a Creative Technologist Based in London with 3+ years experience of working
+                  Dan Adeyemi is a Creative Technologist Based in London with 3+ years experience working
                   in digital media.
-                  Educated at Goldsmiths University, there he did 3 years of a Undergraduate in Creative Computing.
                 </p>
                  <p className="modal-description-text">
-                  Dan started his professional career at E-Commerce company Net-A-Porter, working there as a Junior Web Producer. His
-                  role there included producing their online magazine <i>The Edit</i> and homepage maintenance.
-                  He then moved to Style/Fashion bible Dazed Digital working there as a Web Developer. There he worked on various commercial 
-                  projects and worked on the styling/build for the AnOther Magazine website relaunch. Currently, he os at Shortlist Media. 
-                  Working as a Creative Technologist. Here he overlooks all their brands, advising, prototyping and building new and current web
-                  technology for their various brands.
-                </p>
+                  Dan is equipped with proficient skills to advise, create and manage digital projects in a creative environment while having a full stack of programming skills.
+                  Previous employers include Dazed Media, Net-A-Porter & WCRS(Apple Account).
+                 </p>
                  <p className="modal-description-text">
                   Dan has also done freelance work in Graphic Design, Web Development &amp; Consultancy for various brands.
                   He is also the founder of creative agency <i>A Sense Of Scale</i>.
